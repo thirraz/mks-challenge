d86c77d1d73807d208a2960aa444546727a95ed3
Thiago Souza <thiagofernandosouza20@gmail.com>
2024-04-28 17:11:16 -0300

    update: loading component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import ProductCard from "./components/ProductCard"
 import ProductsGrid from "./components/ProductsGrid"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import SpinnerIcon from "./components/SpinnerIcon"
 
 import { ProductData } from "./types/ProductData"
 import { useProductData } from "./features/products/useProductsData"
@@ -10,13 +11,13 @@ import { StyledApp } from "./styled-components/app/AppStyles"
 export default function App() {
 	const { products, isFetchingProducts } = useProductData()
 
-	if (isFetchingProducts) console.log("loading...")
+	if (isFetchingProducts) return <SpinnerIcon />
 
 	return (
 		<StyledApp>
 			<Header />
 			<ProductsGrid>
-				{products?.products?.map((product: ProductData) => (
+				{products.products?.map((product: ProductData) => (
 					<ProductCard {...product} key={product.id} />
 				))}
 			</ProductsGrid>
